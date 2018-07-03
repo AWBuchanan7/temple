ae0f7db8bf3b61f09b4f708829121a7dccb93f9e
Aubrey Buchanan <buchananaubrey7@gmail.com>
2018-07-03 13:49:41 -0600

    Update ModLoader.js

--- ModLoader.js
+++ ModLoader.js
@@ -3,6 +3,7 @@ Duktape.modSearch = function (id) {
     return res.toString();
 }
 
+// Import Temple
 var CAMERA = require('Game/Camera');
 const camera = CAMERA.GameCamera();
 var INPUT = require('Game/Input');
@@ -10,9 +11,11 @@ const player1 = INPUT.Player1();
 const player3 = INPUT.Player3();
 const button = require('Game/Button');
 
+// Import Mods
 var CAMERAMOD = require('ExampleMods/CameraController.mod');
 console.log('Camera Control loaded.')
 
+// Run Mods
 while (true) {
   CAMERAMOD.Mod();
 }
