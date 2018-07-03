45792b336f3f10e2292955d5f3a424d9165a053f
Aubrey Buchanan <buchananaubrey7@gmail.com>
2018-07-03 09:35:50 -0600

    Update ModLoader.js

--- ModLoader.js
+++ ModLoader.js
@@ -1,1 +1,10 @@
+Duktape.modSearch = function (id) {
+    var res = fs.readFile('C:/Users/Walker/OneDrive/Games/SuperSaix Mod Manager/Util/Ocarina of Time/Scripts/' + id + '.js');
+    return res.toString();
+}
 
+var CAMERAMOD = require('CameraController.mod');
+
+while (true) {
+  CAMERAMOD.Mod();
+}
