607994ebf13a8323c8b92fec053b28093198ef5b
Aubrey Buchanan <buchananaubrey7@gmail.com>
2018-07-03 12:48:30 -0600

    Update CameraController.mod.js

--- ExampleMods/CameraController.mod.js
+++ ExampleMods/CameraController.mod.js
@@ -12,16 +12,17 @@ const button = require('Button');
 
 
 var rotation_amount = 768;
+var roll_amount = 6;
 
 exports.Mod = function() {
     if ((!player1.IsPressingButton(button.Z)) && camera.IsFollowingLink()) {
         
         if (player3.IsPressingButton(button.C_UP)) {
-            camera.Roll_Up();
+            camera.Roll_Up(roll_amount);
         }
 
         if (player3.IsPressingButton(button.C_RIGHT_UP)) {
-            camera.Roll_Up();
+            camera.Roll_Up(roll_amount);
             camera.Rotate_Right(rotation_amount);
         }
 
@@ -30,17 +31,17 @@ exports.Mod = function() {
         }
 
         if (player3.IsPressingButton(button.C_RIGHT_DOWN)) {
-            camera.Roll_Down();
+            camera.Roll_Down(roll_amount);
             camera.Rotate_Right(rotation_amount);
         }
 
         if (player3.IsPressingButton(button.C_DOWN)) {
-            camera.Roll_Down();
+            camera.Roll_Down(roll_amount);
         }
 
         if (player3.IsPressingButton(button.C_LEFT_DOWN)) {
             camera.Rotate_Left(rotation_amount);
-            camera.Roll_Down();
+            camera.Roll_Down(roll_amount);
         }
 
         if (player3.IsPressingButton(button.C_LEFT)) {
@@ -49,7 +50,7 @@ exports.Mod = function() {
 
         if (player3.IsPressingButton(button.C_LEFT_UP)) {
             camera.Rotate_Left(rotation_amount);
-            camera.Roll_Up();
+            camera.Roll_Up(roll_amount);
         }
 
         player3.SetZero();
