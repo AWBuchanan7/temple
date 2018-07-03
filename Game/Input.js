e2ea7f101fbc8ee8f08637c5b4c5cec20a271da2
Aubrey Buchanan <buchananaubrey7@gmail.com>
2018-07-03 12:16:55 -0600

    Update Input.js

--- Game/Input.js
+++ Game/Input.js
@@ -1,26 +1,37 @@
 var Player1 = function() {
     this.input_player1 = 0;
-    mem.bindvar(this, 0x801C84B4, 'input_player1', u16);
+    mem.bindvar(this, 0x801C84B4, 'input_player1_A', u16);
+    mem.bindvar(this, 0x801C84B5, 'input_player1_B', u16);
 
     this.IsPressingButton();
 };
 
 var Player3 = function() {
     this.input_player3 = 0;
-    mem.bindvar(this, 0x801C84E4, 'input_player3', u16);
+    mem.bindvar(this, 0x801C84E4, 'input_player3_A', u16);
+    mem.bindvar(this, 0x801C84E5, 'input_player3_B', u16);
 
     this.IsPressingButton();
 };
 
-//sync the camera rotations
 Player1.prototype.IsPressingButton = function(button) {
-    // console.log("button press: (1)-(" + button + ")" + (input_player1 == button));
-    return this.input_player1 == button;
+    if (button.type == 1) {
+        return this.input_player1_A == button.value;
+    } else if (button.type == 2) {
+        return this.input_player1_B == button.value;
+    }
+    
+    return false;   
 }
 
-//sync the camera rotations
 Player3.prototype.IsPressingButton = function(button) {
-    return this.input_player3 == button;
+    if (button.type == 1) {
+        return this.input_player3_A == button.value;
+    } else if (button.type == 2) {
+        return this.input_player3_B == button.value;
+    }
+    
+    return false;    
 }
 
 Player3.prototype.SetZero = function() {
