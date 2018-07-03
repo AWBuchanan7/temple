92ea2259edb439726a9e243ee9da039ddbf99348
Aubrey Buchanan <buchananaubrey7@gmail.com>
2018-07-03 09:51:48 -0600

    Update ModLoader.js

--- ModLoader.js
+++ ModLoader.js
@@ -1,5 +1,5 @@
 Duktape.modSearch = function (id) {
-    var res = fs.readFile('C:/Users/Walker/OneDrive/Games/SuperSaix Mod Manager/Util/Ocarina of Time/Scripts/' + id + '.js');
+    var res = fs.readFile('C:/Users/Walker/OneDrive/Games/SuperSaix Mod Manager/Util/Ocarina of Time/Scripts/ExampleMods/' + id + '.js');
     return res.toString();
 }
 
