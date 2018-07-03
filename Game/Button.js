8af5942dacec0a5f3ce42e7c28e8141562c43651
Aubrey Buchanan <buchananaubrey7@gmail.com>
2018-07-03 11:54:21 -0600

    Update Button.js

--- Game/Button.js
+++ Game/Button.js
@@ -7,7 +7,18 @@ var Buttons = function() {
     this.C_RIGHT_DOWN = 5;
     this.C_LEFT_UP = 10;
     this.C_LEFT_DOWN = 6;
+    
+    this.A = 4000;
+    this.B = 8000;
     this.Z = 2000;
+    this.R = 10;
+    this.L = 20;
+    this.START = 100;
+
+    this.D_RIGHT = 100;
+    this.D_UP = 800;
+    this.D_LEFT = 200;
+    this.D_DOWN = 400;
 }
 
 module.exports = function() {
