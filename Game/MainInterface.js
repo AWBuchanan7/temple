9c008180b435a0ed44df2409f2c1cea992c55814
Aubrey Buchanan <buchananaubrey7@gmail.com>
2018-07-04 11:18:58 -0600

    Create MainInterface.js

---
+++ Game/MainInterface.js
@@ -0,0 +1,11 @@
+/*
+ * Refer to: https://wiki.cloudmodding.com/oot/Cheat:Interface
+ */
+
+var Interface = function() {
+
+};
+
+exports.MainInterface = function() {
+    return new Interface();
+}
