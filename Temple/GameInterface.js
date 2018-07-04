e4d8943f409823eec601de14a68fa17e95cac0bf
Aubrey Buchanan <buchananaubrey7@gmail.com>
2018-07-04 13:22:28 -0600

    Update and rename MainInterface.js to GameInterface.js

--- Temple/GameInterface.js
+++ Temple/GameInterface.js
@@ -8,6 +8,14 @@
  * Refer to: https://wiki.cloudmodding.com/oot/Cheat:Interface
  */
 
+// magic meter size.
+// D01C84B4 0001
+// 8011B9C5 0000
+// D01C84B4 0001
+// 8011B9C4 00xx
+// xx can be : 00 - Tiny; 02 - Extra Large; 03- Off screen
+
+
 var GameInterface = function() {
 
 };
