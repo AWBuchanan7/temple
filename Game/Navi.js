7671e2f0ac1314a228f8135454ea760a1a51d55b
Aubrey Buchanan <buchananaubrey7@gmail.com>
2018-07-04 11:21:18 -0600

    Create Navi.js

---
+++ Game/Navi.js
@@ -0,0 +1,17 @@
+
+
+var Navi = function() {
+ /*
+  * Navi's Color
+  * 811E2154 43??
+  * 811E2158 43??
+  * 811E215B 43??
+  *
+  *
+  */
+};
+
+
+exports.Navi = function() {
+    return new Navi();
+}
